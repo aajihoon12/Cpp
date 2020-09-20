1a8cc2649830eb66bc30890af9e03bdb9b4acf29
aajihoon12 <aajihoon12@gmail.com>
2020-09-20 14:50:12 +0900

    CH02

---
+++ CH02_UsingDcl2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+main() {
+    int num=20;
+    cout<<"Hello World!"<<endl;
+    cout<<"Hello "<<"World!"<<endl;
+    cout<<num<<' '<<'A';
+    cout<<' '<<3.14<<endl;
+    return 0;
+}
