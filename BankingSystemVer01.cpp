9b11ddbbea6de59401d4f9fee54dddd9374acbc2
aajihoon12 <aajihoon12@gmail.com>
2020-11-12 22:15:02 +0900

    OOP_PROJECT v1.2 -> v0.5
    1.ÁÖ version Á¤º¸¸¦ ´ٽÃ 0À¸·Î Á¶Á¤Ç߽À´ϴÙ.
    2.Áּ®Á¤º¸¿¡ ´ëÇÑ Àڼ¼ÇÑ ÇʱâÀÇ Çʿ伺 ÀÎÁöÇ߽À´ϴÙ.
    3.ÇÁ·α׷¥ÀÇ ±â´É ȤÀº È帧À» ±¸¼ºÇϱâ À§ÇÑ HandlerClass¸¦ Á¤ÀÇÇ߽À´ϴÙ.

--- BankingSystemVer01.cpp
+++ BankingSystemVer01.cpp
@@ -10,12 +10,6 @@
 using namespace std;
 const int NAME_LEN=20;
 
-void ShowMenu(void);       // 메뉴출력
-void MakeAccount(void);    // 계좌개설을 위한 함수
-void DepositMoney(void);       // 입    금
-void WithdrawMoney(void);      // 출    금
-void ShowAllAccInfo(void);     // 잔액조회
-
 enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
 
 class Account
@@ -65,16 +59,103 @@ public:
 	}	
 };
 
-Account * accArr[100];   // Account 저장을 위한 배열
-int accNum=0;        // 저장된 Account 수
+class AccountHandler {
+	Account * accArr[100];
+	int accNum;
+	AccountHandler(const AccountHandler &copy) { }
+public:
+	AccountHandler() : accNum(0);
+
+	void ShowMenu(void) {
+		cout<<"-----Menu------"<<endl;
+		cout<<"1. 계좌개설"<<endl;
+		cout<<"2. 입    금"<<endl;
+		cout<<"3. 출    금"<<endl;
+		cout<<"4. 계좌정보 전체 출력"<<endl;
+		cout<<"5. 프로그램 종료"<<endl;
+	}
+
+	void MakeAccount(void) {
+			int id;
+			char *name;
+			int balance;
+			
+			cout<<"[계좌개설]"<<endl;
+			cout<<"계좌ID: ";	cin>>id;
+			cout<<"이  름: ";	cin>>name;
+			cout<<"입금액: ";	cin>>balance;
+			cout<<endl;
+
+			accArr[accNum++]=new Account(id, balance, name);
+		}
+
+	void DepositMoney(void){
+		int money;
+		int id;
+		cout<<"[입    금]"<<endl;
+		cout<<"계좌ID: ";	cin>>id;
+		cout<<"입금액: ";	cin>>money;
+		
+		for(int i=0; i<accNum; i++)
+		{
+			if(accArr[i]->GetAccID()==id)
+			{
+				accArr[i]->Deposit(money);
+				cout<<"입금완료"<<endl<<endl;
+				return;
+			}
+		}
+		cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
+	}
+
+	void WithdrawMoney(void){
+		int money;
+		int id;
+		cout<<"[출    금]"<<endl;
+		cout<<"계좌ID: ";	cin>>id;
+		cout<<"출금액: ";	cin>>money;
+		
+		for(int i=0; i<accNum; i++)
+		{
+			if(accArr[i]->GetAccID()==id)
+			{
+				if(accArr[i]->Withdraw(money)==0)
+				{
+					cout<<"잔액부족"<<endl<<endl;
+					return;
+				}
+
+				cout<<"출금완료"<<endl<<endl;
+				return;
+			}
+		}
+		cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
+	}
+
+	void ShowAllAccInfo(void)	
+	{
+		for(int i=0; i<accNum; i++)
+		{
+			accArr[i]->ShowAccInfo();
+			cout<<endl;
+		}
+	}
+
+	~AccountHandler() {
+		for(int i=0;i<accNum;i++) {
+			delete accArr[i];
+		}
+	}
+};
 
 int main(void)
 {
+	AccountHandler handler;
 	int choice;
-	
+
 	while(1)
 	{
-		ShowMenu();
+		handler.ShowMenu();
 		cout<<"선택: "; 
 		cin>>choice;
 		cout<<endl;
@@ -82,16 +163,16 @@ int main(void)
 		switch(choice)
 		{
 		case MAKE:
-			MakeAccount(); 
+			handler.MakeAccount(); 
 			break;
 		case DEPOSIT:
-			DepositMoney(); 
+			handler.DepositMoney(); 
 			break;
 		case WITHDRAW: 
-			WithdrawMoney(); 
+			handler.WithdrawMoney(); 
 			break;
 		case INQUIRE:
-			ShowAllAccInfo(); 
+			handler.ShowAllAccInfo(); 
 			break;
 		case EXIT:
 			return 0;
@@ -100,86 +181,6 @@ int main(void)
 		}
 	}
 
-	for(int i=0; i<accNum; i++)
-		delete accArr[i];
 	return 0;
 }
 
-void ShowMenu(void)
-{
-	cout<<"-----Menu------"<<endl;
-	cout<<"1. 계좌개설"<<endl;
-	cout<<"2. 입    금"<<endl;
-	cout<<"3. 출    금"<<endl;
-	cout<<"4. 계좌정보 전체 출력"<<endl;
-	cout<<"5. 프로그램 종료"<<endl;
-}
-
-void MakeAccount(void) 
-{
-	int id;
-	char name[NAME_LEN];
-	int balance;
-	
-	cout<<"[계좌개설]"<<endl;
-	cout<<"계좌ID: ";	cin>>id;
-	cout<<"이  름: ";	cin>>name;
-	cout<<"입금액: ";	cin>>balance;
-	cout<<endl;
-
-	accArr[accNum++]=new Account(id, balance, name);
-}
-
-void DepositMoney(void)
-{
-	int money;
-	int id;
-	cout<<"[입    금]"<<endl;
-	cout<<"계좌ID: ";	cin>>id;
-	cout<<"입금액: ";	cin>>money;
-	
-	for(int i=0; i<accNum; i++)
-	{
-		if(accArr[i]->GetAccID()==id)
-		{
-			accArr[i]->Deposit(money);
-			cout<<"입금완료"<<endl<<endl;
-			return;
-		}
-	}
-	cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
-}
-
-void WithdrawMoney(void)
-{
-	int money;
-	int id;
-	cout<<"[출    금]"<<endl;
-	cout<<"계좌ID: ";	cin>>id;
-	cout<<"출금액: ";	cin>>money;
-	
-	for(int i=0; i<accNum; i++)
-	{
-		if(accArr[i]->GetAccID()==id)
-		{
-			if(accArr[i]->Withdraw(money)==0)
-			{
-				cout<<"잔액부족"<<endl<<endl;
-				return;
-			}
-
-			cout<<"출금완료"<<endl<<endl;
-			return;
-		}
-	}
-	cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
-}
-
-void ShowAllAccInfo(void)	
-{
-	for(int i=0; i<accNum; i++)
-	{
-		accArr[i]->ShowAccInfo();
-		cout<<endl;
-	}
-}
