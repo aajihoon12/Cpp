b60d9384175e3f852612e5cc69ec17d50b7a49ca
aajihoon12 <aajihoon12@gmail.com>
2020-12-28 18:54:45 +0900

    CH14

--- CH14_PointTemplateFriendFunction.cpp
+++ CH14_PointTemplateFriendFunction.cpp
@@ -5,13 +5,29 @@ template <class T>
 class Point {
     T xpos, ypos;
 public:
-    Point(T x=0, T y=0);
-    void ShowPosition() const;
+    Point(T x=0, T y=0): xpos(x), ypos(y) {}
+    void ShowPosition() const {
+        cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
+    }
 
-    friend Point<int> operator+ (const Point<int>&);
+    friend Point<int> operator+ (const Point<int>&, const Point<int>&);
     friend ostream& operator<< (ostream&, const Point<int>&);
 };
 
-Point<int> operator+ (const Point<int>& pos1) {
-    return Point<int>(xpos+pos1.xpos, ypos+pos1.ypos);
+Point<int> operator+ (const Point<int>& pos1, const Point<int>& pos2) {
+    return Point<int>(pos1.xpos+pos2.xpos, pos1.ypos+pos2.ypos);
 }
+
+ostream& operator<< (ostream& os, const Point<int>& pos1) {
+    pos1.ShowPosition();
+    return os;
+}
+
+main() {
+    Point<int> pos1(2, 4);
+    Point<int> pos2(4, 8);
+    Point<int> pos3=pos1+pos2;
+    cout<<pos1<<pos2<<pos3;
+    return 0;
+}
+
